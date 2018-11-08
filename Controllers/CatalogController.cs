431affc173f8a392a6d8b3b9fe47cf6003de7ba8
Jurid <juri.d@mail.ru>
2018-11-08 16:41:26 +0300

    in work

---
+++ Controllers/CatalogController.cs
@@ -0,0 +1,72 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shop.Data;
+using Shop.Models;
+
+namespace Shop.Controllers
+{
+    [Route("catalog")]
+    public class CatalogController : Controller
+    {
+        private readonly ShopContext _db;
+
+        public CatalogController(ShopContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _db.Products.ToListAsync();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        [Route("add_in_basket/{id:int}")]
+        public async Task<IActionResult> Product(int? id)
+        {
+            if (!id.HasValue)
+                return View();
+
+            var product = await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
+            return View(product);
+        }
+
+        [HttpPost]
+        [Route("save")]
+        public async Task<IActionResult> Save(Product product)
+        {
+            if (product.Id == default(int))
+            {
+                _db.Products.Add(product);
+            }
+            else
+            {
+                _db.Products.Attach(product);
+                _db.Entry(product).State = EntityState.Modified;
+            }
+
+            await _db.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("del_from_basket/{id:int}")]
+        public async Task<IActionResult> Del(int? id)
+        {
+            var product = await _db.Products.FirstOrDefaultAsync(x => x.Id == id);
+            _db.Products.Remove(product);
+            //_db.Entry(product).State = EntityState.Modified;
+
+            await _db.SaveChangesAsync();
+
+            //return View();
+            return RedirectToAction("Index");
+        }
+    }
+}
